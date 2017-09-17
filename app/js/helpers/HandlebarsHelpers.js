ae3e2f8b499739880d13fb3fb3c31cd245dbc5e7
Sarah Dayan <dayan.sarah@gmail.com>
2017-09-18 00:27:56 +0200

    Document Handlebars Helpers

--- app/js/helpers/HandlebarsHelpers.js
+++ app/js/helpers/HandlebarsHelpers.js
@@ -1,7 +1,22 @@
 define(function() {
 
+	'use strict';
+
+	/**
+	 * The handlebar helpers
+	 * @namespace
+	 */
 	var HandlebarsHelpers = (function() {
 		return {
+			/**
+			 * @function compare
+			 * @description Compares two values
+			 * @param {(string|number)} leftValue - the first value to compare.
+			 * @param {string} operator - the operator to use.
+			 * @param {(string|number)} rightValue - the second value to compare.
+			 * @param {object} options - an object of options.
+			 * @returns {boolean}
+			 */
 			compare: function(leftValue, operator, rightValue, options) {
 				if (arguments.length < 3) throw new Error('Handlerbars Helper \'compare\' needs 2 parameters');
 
